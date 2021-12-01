59888e2162e41b9cabe9e33de1aa7a431b4a6e00
Andy Nguyen <andy-nguyen@live.ca>
2021-11-30 20:45:04 -0800

    finished?

--- dog.js
+++ dog.js
@@ -17,7 +17,7 @@ app.use("/media", express.static("public/media"));
 
 app.use(session(
     {
-        secret: "extra text that no one will guess",
+        secret: "yeah i dont know",
         name: "dogSessionID",
         resave: false,
         saveUninitialized: true
@@ -33,9 +33,6 @@ app.get("/", function (req, res) {
     } else {
 
         let doc = fs.readFileSync("./app/html/index.html", "utf8");
-
-        res.set("Server", "A Legitimate Engine");
-        res.set("X-Powered-By", "Hamster Wheels");
         res.send(doc);
 
     }
@@ -45,35 +42,23 @@ app.get("/", function (req, res) {
 
 app.get("/profile", function (req, res) {
 
-    // check for a session first!
+    // check for a session first
     if (req.session.loggedIn) {
 
         let profile = fs.readFileSync("./app/html/profile.html", "utf8");
         let profileDOM = new JSDOM(profile);
 
-        // great time to get the user's data and put it into the page!
-        //profileDOM.window.document.getElementsByTagName("title")[0].innerHTML
-        //    = req.session.name + "'s Profile";
-        //profileDOM.window.document.getElementById("profile_name").innerHTML
-        //    = "Welcome back " + req.session.name;
-
-
-
-
-
-
-
-        res.set("Server", "Wazubi Engine");
-        res.set("X-Powered-By", "Wazubi");
         //serialize will convert profileDOM, which is a data structure, into a workable HTML format.
         //console.log(profileDOM.serialize());
 
-        //const tablee = profileDOM.window.document.createElement("table");
+        const tablee = profileDOM.window.document.createElement("table");
         const dataTable = profileDOM.window.document.createElement("table");
         getData(req, function (result, tableResult) {
             //console.log(result);
             if (result != undefined && tableResult != undefined) {
                 for (let i = 0; i < result.length; i++) {
+
+                    // Header and Footer content filling
                     profileDOM.window.document.getElementById("name-here").innerHTML = result[i].name;
                     profileDOM.window.document.getElementById("sub-here").innerHTML = "Subscription days left: " + result[i].sub_days_remaining;
                     profileDOM.window.document.getElementById("email-here").innerHTML = result[i].email;
@@ -81,28 +66,80 @@ app.get("/profile", function (req, res) {
                     profileDOM.window.document.getElementById("footercontent").style.backgroundColor = result[i].color;
                     let count = result[i].extra;
                     let str = "<div>";
-                    for (let i = 0; i < count; i++){
+                    for (let i = 0; i < count; i++) {
                         str += `<span class="dogico">&nbsp;</span>`;
                     }
                     str += "</div>";
                     profileDOM.window.document.getElementById("dogicon-here").innerHTML = str;
-                    //let str = "<tr><td>" + "Name: " + result[i].name;
-                    //str += " Email: " + result[i].email;
-                    //str +=  " Result: " + result[i].password + "</td></tr>";
-                    //tablee.innerHTML += str;
-                    //console.log(str);
-                }
-                //profileDOM.window.document.getElementById("user-table").appendChild(tablee);
 
-                for (let i = 0; i < tableResult.length; i++) {
-                    let str = "<tr><td>" + "Name: " + tableResult[i].breed;
-                    str += " Height: " + tableResult[i].height;
-                    str +=  " Weight: " + tableResult[i].weight + "</td></tr>";
-                    dataTable.innerHTML += str;
-                    //console.log(str);
+                    // User info table
+                    tablee.innerHTML = makeUserTable(result);
                 }
-                profileDOM.window.document.getElementById("data-table").appendChild(dataTable);
-                //console.log(tableResult);
+                profileDOM.window.document.getElementById("user-table").appendChild(tablee);
+                
+                //Creating the cards
+                for (let i = 0; i < tableResult.length; i++) {
+                    let card = profileDOM.window.document.createElement("span");
+                    card.className = "card";
+
+                    let image = profileDOM.window.document.createElement("img");
+                    image.className = "dog-pic";
+
+                    //I only have 3 dog pictures, so fill the rest with dummy image
+                    if (i < 3) {
+                        let imageLocation = "/img/dog" + (i + 1) + ".jpg";
+                        image.src = imageLocation;
+                    } else {
+                        image.src = "https://dummyimage.com/400x300/000/fff";
+                    }
+
+                    image.alt = "image";
+                    card.appendChild(image);
+
+                    let cap = profileDOM.window.document.createElement("caption");
+                    cap.className = "doglabel";
+                    cap.innerHTML = tableResult[i].breed;
+
+                    let infotable = profileDOM.window.document.createElement("table");
+                    let tablehead = profileDOM.window.document.createElement("thead");
+                    let row = profileDOM.window.document.createElement("tr");
+                    let rowdata1 = profileDOM.window.document.createElement("td");
+                    let rowdata2 = profileDOM.window.document.createElement("td");
+                    let rowdata3 = profileDOM.window.document.createElement("td");
+                    
+                    rowdata1.innerHTML = "Weight";
+                    row.appendChild(rowdata1);
+                    rowdata2.innerHTML = "Height";
+                    row.appendChild(rowdata2);
+                    rowdata3.innerHTML = "Lifespan";
+                    row.appendChild(rowdata3);
+
+                    infotable.appendChild(cap);
+                    tablehead.appendChild(row);
+                    infotable.appendChild(tablehead);
+
+                    let row2 = profileDOM.window.document.createElement("tr");
+                    let rowdata1_1 = profileDOM.window.document.createElement("td");
+                    let rowdata1_2 = profileDOM.window.document.createElement("td");
+                    let rowdata1_3 = profileDOM.window.document.createElement("td");
+                    
+                    rowdata1_1.innerHTML = tableResult[i].weight;
+                    row2.appendChild(rowdata1_1);
+                    rowdata1_2.innerHTML = tableResult[i].height;
+                    row2.appendChild(rowdata1_2);
+                    rowdata1_3.innerHTML = tableResult[i].lifespan;
+                    row2.appendChild(rowdata1_3);
+
+                    infotable.appendChild(row2);
+                    card.appendChild(infotable);
+
+                    let extrainfo = profileDOM.window.document.createElement("div");
+                    extrainfo.className = "dog-extra";
+                    extrainfo.innerHTML = tableResult[i].extra_info;
+                    card.appendChild(extrainfo);
+
+                    profileDOM.window.document.getElementById("data-table").appendChild(card);
+                }
                 //this part neeeds to be here or else getData results will get overwritten
                 res.send(profileDOM.serialize());
 
@@ -178,7 +215,7 @@ function authenticate(email, pwd, callback) {
         host: "localhost",
         user: "root",
         password: "",
-        database: "dogtest"
+        database: "dog"
     });
     connection.connect();
     connection.query(
@@ -228,8 +265,8 @@ async function init() {
         password: "",
         multipleStatements: true
     });
-    const createDBAndTables = `CREATE DATABASE IF NOT EXISTS dogtest;
-        use dogtest;
+    const createDBAndTables = `CREATE DATABASE IF NOT EXISTS dog;
+        use dog;
         CREATE TABLE IF NOT EXISTS user (
         ID int NOT NULL AUTO_INCREMENT,
         name varchar(30),
@@ -245,7 +282,7 @@ async function init() {
             height varchar(30),
             weight varchar(30),
             lifespan varchar(30),
-            extra_info varchar(30),
+            extra_info varchar(50),
             PRIMARY KEY (ID));`;
     await connection.query(createDBAndTables);
 
@@ -270,8 +307,15 @@ async function init() {
         let dataRecords = "insert into data (breed, height, weight, lifespan, extra_info) values ?";
         let dataValues = [
             ["Afador", "20-29 inches", "50-75 pounds", "10-12 years", "Smart and athletic"],
-            ["Afador", "20-29 inches", "50-75 pounds", "10-12 years", "Smart and athletic"],
-            ["Afador", "20-29 inches", "50-75 pounds", "10-12 years", "Smart and athletic"]
+            ["Shiba Inu", "13-17 inches", "17-23 pounds", "12-16 years", "Is that meme dog"],
+            ["German Shepherd", "22-26 inches", "75-95 pounds", "10-14 years", "Employed in police"],
+            ["Cavador", "18-24 inches", "22-55 pounds", "10-14 years", "Playful"],
+            ["Chihuahua", "6-9 inches", "3-6 pounds", "10-18 years", "Is the smallest dog breed"],
+            ["Maltese", "8-10 inches", "4-7 pounds", "12-15 years", "Doesn't shed much"],
+            ["Mastiff", "27-32 inches", "130-220 pounds", "6-10 years", "Used to be war dogs"],
+            ["Daniff", "27-33 inches", "115-190 pounds", "8-12 years", "A \"gentle giant\""],
+            ["Syberian Husky", "20-23 inches", "33-60 pounds", "12-15 years", "May jump fences and escape"],
+            ["Labrador Retriever", "21-24 inches", "55-80 pounds", "10-12 years", "Historically a fisherman's helper"],
         ];
         await connection.query(dataRecords, [dataValues]);
     }
@@ -280,6 +324,7 @@ async function init() {
     console.log("Listening on port " + port + "!");
 }
 
+//get the user data and all of the contents of the other table
 function getData(req, callback) {
     var mysql = require('mysql2');
     var userData;
@@ -289,28 +334,50 @@ function getData(req, callback) {
         host: "localhost",
         user: "root",
         password: "",
-        database: "dogtest",
+        database: "dog",
         multipleStatements: true
     });
 
     connection.connect(function (err) {
         if (err) throw err;
-        connection.query("SELECT * FROM user WHERE email = ?", [req.session.email], 
-        function (err, result, fields) {
-            if (err) throw err;
-            //console.log(result);
-            userData = result;
-            connection.query("SELECT * FROM data", function (err, result, fields) {
+        connection.query("SELECT * FROM user WHERE email = ?", [req.session.email],
+            function (err, result, fields) {
                 if (err) throw err;
                 //console.log(result);
-                tableData = result;
-                return callback(userData, tableData);
+                userData = result;
+                connection.query("SELECT * FROM data", function (err, result, fields) {
+                    if (err) throw err;
+                    //console.log(result);
+                    tableData = result;
+                    return callback(userData, tableData);
+                });
             });
-        });
 
-        
+
     });
 
+}
+
+//this function will only get an array of size 1
+function makeUserTable(data) {
+    let str = "<caption id=\"tablecaption\"> User Information </caption>";
+    str += "<thead id=\"user-header-rows\"><tr><td>" + "&nbsp;ID&nbsp;" + "</td>";
+    str += "<td>" + " Name " + "</td>";
+    str += "<td>" + " Email " + "</td>";
+    str += "<td>" + " Password " + "</td>";
+    str += "<td>" + " Dog icons " + "</td>";
+    str += "<td>" + " Footer Color" + "</td>";
+    str += "<td>" + " Subscription days left " + "</td> </tr> </thead>";
+
+    str += "<tr id=\"user-data-rows\"><td>" + data[0].ID + "</td>";
+    str += "<td>" + data[0].name + "</td>";
+    str += "<td>" + data[0].email + "</td>";
+    str += "<td>" + data[0].password + "</td>";
+    str += "<td>" + data[0].extra + "</td>";
+    str += "<td>" + data[0].color + "</td>";
+    str += "<td>" + data[0].sub_days_remaining + "</td></tr>";
+    //console.log(str);
+    return str;
 }
 
 // RUN SERVER
